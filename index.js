b420880ae8f309df64743c8513b4569951922afa
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-17 10:08:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer ;
+const leastFavouriteCustomer='maybe its bob'
+console.log(customerName)
+
+function upperCaseCustomerName() {
+    // let customerName='bob'
+    customerName= customerName.toUpperCase()
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer='not bob'
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = "it\'s now bob"
+}
+
+
